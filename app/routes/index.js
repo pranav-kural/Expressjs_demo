66fb0883773903d0893256431c19de18eef6b45d
Pranav Kural <pranavkural042@gmail.com>
2017-01-21 15:56:58 -0500

    Added data for Home page for the Template

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,17 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index');
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+
+  data.speakers.forEach(function(speaker) {
+    pagePhotos = pagePhotos.concat(speaker.artwork);
+  });
+  res.render('index', {
+    pageTitle: 'Home',
+    pageId: 'home',
+    artwork: pagePhotos
+  });
 });
 
 module.exports = router;
