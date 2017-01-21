ce3cfedee8ff221ee4eeafad241b5730c525806f
Pranav Kural <pranavkural042@gmail.com>
2017-01-21 18:00:44 -0500

    Edited pageId to pageID and added speakers to template data

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,13 +4,15 @@ let router = express.Router();
 router.get('/', function(req, res) {
   var data = req.app.get('appData');
   var pagePhotos = [];
+  var pageSpeakers = data.speakers;
 
   data.speakers.forEach(function(speaker) {
     pagePhotos = pagePhotos.concat(speaker.artwork);
   });
   res.render('index', {
     pageTitle: 'Home',
-    pageId: 'home',
+    pageID: 'home',
+    speakers: pageSpeakers,
     artwork: pagePhotos
   });
 });
